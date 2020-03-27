69e8f86e44dbac89fbbbdd7a6766aaa5a23ecb17
Nancy Alter <nancy.alter@gmail.com>
2020-03-27 16:57:24 -0400

    update favicon
    update favicon

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "index.html", "index.js", "favicon.ico", "styles.css"];
+const FILES_TO_CACHE = ["/", "index.html", "index.js", "favicon.jpg", "styles.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
